f8fb7040a7480c763f48158536278644e142ceec
dmmkimani <95884443+dmmkimani@users.noreply.github.com>
2025-02-11 15:00:15 +0300

    Update based on recent experiences

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,8 @@
 
     <div style="margin-top: 75px;">
         <p class="violet">I'm </p><p class="gold">David Mike Mwaura Kimani </p><p class="violet">but you can call me </p><p class="gold">Mike </p><p class="violet">(everyone does).</p><br>
-        <p class="violet">I've recently completed my </p><p class="gold">MSc in Advanced Computer Science with Distinction </p><p class="violet">and</p><br>
-        <p class="violet">am now eager to </p><p class="gold">apply my skills </p><p class="violet">and </p><p class="gold">continuously learn </p><p class="violet">in a </p><p class="gold">professional environment</p><p class="violet">.</p>
+        <p class="violet">I have an </p><p class="gold">MSc in Advanced Computer Science with Distinction </p><p class="violet">and</p><br>
+        <p class="violet">I am always eager to </p><p class="gold">apply my skills </p><p class="violet">and </p><p class="gold">continuously learn </p><p class="violet">in a </p><p class="gold">professional environment</p><p class="violet">.</p>
     </div>
 
     <div style="margin-top: 75px;">
@@ -29,6 +29,7 @@
         @include('skills')
     </div>
 
+    <!--
     <div style="margin:auto; width: fit-content">
         <a href="https://drive.google.com/file/d/14Ugg9bGTwIU2a1D_qXVFXu9o_o3N0p1P/view?usp=embed_googleplus" target="_blank">
             <div id="cv" class="rounded cv">
@@ -38,6 +39,7 @@
             </div>
         </a>
     </div>
+    -->
 
     <div style="margin-top: 75px;">
         <p style="color: DarkViolet">...</p>
--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -8,14 +8,32 @@
     <div class="rounded skill" onmouseover="changeText('JavaScript is my strongest language for server-side tasks.')" onmouseout="changeback()">
         <h4>JavaScript</h4>
     </div>
+    <div class="rounded skill" onmouseover="changeText('I really enjoy the specificity TypeScript affords. Learning TypeScript has actually improved my Python skills by enforcing types on variables.')" onmouseout="changeback()">
+        <h4>TypeScript</h4>
+    </div>
     <div class="rounded skill" onmouseover="changeText('I really enjoy diving into mobile app development. Actually, I am currently working on an Android app.')" onmouseout="changeback()">
         <h4>Kotlin</h4>
     </div>
     <div class="rounded skill" onmouseover="changeText('While Dart is conventionally used in Flutter, I\'ve also applied it for server-side development.')" onmouseout="changeback()">
         <h4>Dart</h4>
     </div>
+    <div class="rounded skill" onmouseover="changeText('My favourite frontend technology... for now.')" onmouseout="changeback()">
+        <h4>React</h4>
+    </div>
     <div class="rounded skill" onmouseover="changeText('I have completed numerous projects encompassing both client and server-side development.')" onmouseout="changeback()">
         <h4>Node.js</h4>
+    </div>
+    <div class="rounded skill" onmouseover="changeText('Currently using NestJS as the backend for an exciting application! Will provide details as soon as I can.')" onmouseout="changeback()">
+        <h4>NestJS</h4>
+    </div>
+    <div class="rounded skill" onmouseover="changeText('Currently using Django as the backend for an exciting application! Will provide details as soon as I can.')" onmouseout="changeback()">
+        <h4>Django</h4>
+    </div>
+    <div class="rounded skill" onmouseover="changeText('dmmkimani.com uses plain CSS. But since learning Tailwind CSS, this website needs a revamp.')" onmouseout="changeback()">
+        <h4>Tailwind CSS</h4>
+    </div>
+    <div class="rounded skill" onmouseover="changeText('I love how lightweight Vue.js is. It can be a relatively simple solution to provide that extra touch of reactivity.')" onmouseout="changeback()">
+        <h4>Vue.js</h4>
     </div>
     <div class="rounded skill" onmouseover="changeText('Check out YourSpace below for an insight into a mobile app I developed using Flutter.')" onmouseout="changeback()">
         <h4>Flutter</h4>
@@ -26,32 +44,41 @@
     <div class="rounded skill" onmouseover="changeText('I recognise the importance of a database design that is both efficient and scalable.')" onmouseout="changeback()">
         <h4>PostgreSQL</h4>
     </div>
-    <div class="rounded skill" onmouseover="changeText('Document databases such as Cloud Firestore in particular.')" onmouseout="changeback()">
-        <h4>NoSQL</h4>
+    <div class="rounded skill" onmouseover="changeText('I love the flexibility NoSQL databases like MongoDB allow. It has been instrumental in my efforts to develop an LMS.')" onmouseout="changeback()">
+        <h4>MongoDB</h4>
     </div>
+    <!--
     <div class="rounded skill" onmouseover="changeText('Currently leveraging PHP to build a repair maintenance system.')" onmouseout="changeback()">
         <h4>PHP</h4>
     </div>
+    -->
     <div class="rounded skill" onmouseover="changeText('I always employ Git branching strategies to efficiently manage development tasks and isolate bug fixes.')" onmouseout="changeback()">
         <h4>Git</h4>
     </div>
     <div class="rounded skill" onmouseover="changeText('I have a passion for UX that stems from a want to understand the user.')" onmouseout="changeback()">
         <h4>UX</h4>
     </div>
+    <!--
     <div class="rounded skill" onmouseover="changeText('Go to my GitHub to see the HTML and CSS for this website!')" onmouseout="changeback()">
         <h4>HTML/CSS</h4>
     </div>
+    -->
+    <!--
     <div class="rounded skill" onmouseover="changeText('The Incremental Development Model in particular.')" onmouseout="changeback()">
         <h4>Agile Methodologies</h4>
     </div>
+    -->
     <div class="rounded skill" onmouseover="changeText('JavaScript Object Notation... what can I say?')" onmouseout="changeback()">
         <h4>JSON</h4>
     </div>
     <div class="rounded skill" onmouseover="changeText('I am able to propose, design and implement functionality that provides a solution to a problem.')" onmouseout="changeback()">
         <h4>Problem Solving</h4>
     </div>
+    <div class="rounded skill" onmouseover="changeText('Especially within the realm of frontend design. I always try to encaspulate a company\'s vibe into my UI designs. Or in this case, my vibe.')" onmouseout="changeback()">
+        <h4>Creativity</h4>
+    </div>
     <div class="rounded skill" onmouseover="changeText('Developing projects in groups has enhanced my teamwork and interpersonal communication skills.')" onmouseout="changeback()">
-        <h4>Teamwork & Communication</h4>
+        <h4>Communication</h4>
     </div>
     <!--
     <div class="rounded skill" onmouseover="changeText('Used to unit test the YourSpace UI.')" onmouseout="changeback()">
